161f68267090a2f5213dabe23100c2a1f74f7736
Saurabh Kulkarni <saurabhkulkarni@Saurabhs-MacBook-Pro.local>
2020-08-12 16:19:06 -0400

    checked times with goroutines

--- go/web-crawler/main.go
+++ go/web-crawler/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"./crawler"
+	"fmt"
 	"sync"
+	"strconv"
+	"time"
 )
 
 
@@ -14,6 +17,12 @@ func main() {
 	crawler := crawler.NewCrawler();
 	wg := &sync.WaitGroup{}
 	wg.Add(1);
+	timeStart := time.Now().UnixNano();
 	startCrawler(crawler, wg, "test", 4);
 	wg.Wait();
+	timeEnd := time.Now().UnixNano();
+	timeDiff := timeEnd - timeStart;
+	// without go routing 4724000 ns
+	// with go routines 4750000 ns
+	fmt.Println("Total time taken (in nano sec): " + strconv.FormatInt(timeDiff, 10));
 }
